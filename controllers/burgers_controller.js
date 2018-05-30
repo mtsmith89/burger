a51b3020125ca521c7ca9c4262772328aa3210ab
Travis Smith <Travis.Smith2@gmail.com>
2018-05-30 19:26:42 -0400

    burgers_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,9 +5,9 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
@@ -15,15 +15,42 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  cat.create([
-    "name"
+  burger.create([
+    "burger_name", "devoured"
   ], [
-    req.body.name
+    req.body.burger_name, req.body.devoured
   ], function(result) {
     res.json({ id: result.insertId });
   });
 });
 
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 
 module.exports = router;
