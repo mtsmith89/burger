b98f55f36e2752b86b74cdbfb9b66f9a8d872f51
Travis Smith <Travis.Smith2@gmail.com>
2018-05-30 18:43:07 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES("The Krusty Burger");
+
+INSERT INTO burgers(burger_name)
+VALUES("The Ghost Burger");
+
+INSERT INTO burgers(burger_name)
+VALUES("The Jucy Lucy");
